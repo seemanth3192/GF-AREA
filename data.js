06910642c87900f77c54c3be9c61dfa107939c98
seemanth3192 <seemanthdyc3192@gmail.com>
2025-03-18 11:55:04 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,347 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-copy",
+      "name": "living copy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3224645720792663
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0883908237764714,
+          "pitch": 0.14837208143910274,
+          "rotation": 1.5707963267948966,
+          "target": "1-dining"
+        },
+        {
+          "yaw": -1.877705662355524,
+          "pitch": 0.16507564457234736,
+          "rotation": 4.71238898038469,
+          "target": "2-stair"
+        },
+        {
+          "yaw": -1.3886720765956042,
+          "pitch": 0.15293937639420285,
+          "rotation": 0,
+          "target": "3-foyer"
+        },
+        {
+          "yaw": -1.2547194997093563,
+          "pitch": 0.019081053720000796,
+          "rotation": 1.5707963267948966,
+          "target": "4-kitchen"
+        },
+        {
+          "yaw": -1.4465432334072528,
+          "pitch": 0.017234392926873454,
+          "rotation": 0,
+          "target": "5-courtyard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dining",
+      "name": "dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3224645720792663
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1758827198978246,
+          "pitch": 0.1498232141685989,
+          "rotation": 4.71238898038469,
+          "target": "0-living-copy"
+        },
+        {
+          "yaw": 2.7291223698575404,
+          "pitch": 0.21430783834275857,
+          "rotation": 0.7853981633974483,
+          "target": "2-stair"
+        },
+        {
+          "yaw": -1.163790830343924,
+          "pitch": 0.308873653605227,
+          "rotation": 11.780972450961727,
+          "target": "3-foyer"
+        },
+        {
+          "yaw": -0.9302226820146089,
+          "pitch": 0.07610121395177671,
+          "rotation": 1.5707963267948966,
+          "target": "4-kitchen"
+        },
+        {
+          "yaw": -1.2965253367809861,
+          "pitch": 0.07155874528767825,
+          "rotation": 5.497787143782138,
+          "target": "5-courtyard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-stair",
+      "name": "stair",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3224645720792663
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.740000253606909,
+          "pitch": 0.1492584111862989,
+          "rotation": 0,
+          "target": "3-foyer"
+        },
+        {
+          "yaw": 1.7371929754681839,
+          "pitch": -0.017361559568536578,
+          "rotation": 0,
+          "target": "5-courtyard"
+        },
+        {
+          "yaw": 1.9801628204455515,
+          "pitch": -0.01845415933485306,
+          "rotation": 1.5707963267948966,
+          "target": "4-kitchen"
+        },
+        {
+          "yaw": 2.579583535990391,
+          "pitch": 0.15071129990809595,
+          "rotation": 0.7853981633974483,
+          "target": "1-dining"
+        },
+        {
+          "yaw": -2.034868307525848,
+          "pitch": 0.2635973833827592,
+          "rotation": 5.497787143782138,
+          "target": "0-living-copy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-foyer",
+      "name": "foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3224645720792663
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "5-courtyard"
+        },
+        {
+          "yaw": 0.8123130040067448,
+          "pitch": 0.08387327817373702,
+          "rotation": 1.5707963267948966,
+          "target": "4-kitchen"
+        },
+        {
+          "yaw": 2.700647271907549,
+          "pitch": 0.20215838722923074,
+          "rotation": 4.71238898038469,
+          "target": "1-dining"
+        },
+        {
+          "yaw": -2.5828003430690956,
+          "pitch": 0.20481607995509954,
+          "rotation": 1.5707963267948966,
+          "target": "2-stair"
+        },
+        {
+          "yaw": -3.0504497237261603,
+          "pitch": 0.05793443308009927,
+          "rotation": 5.497787143782138,
+          "target": "0-living-copy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3224645720792663
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7772153896948435,
+          "pitch": 0.09963505214214052,
+          "rotation": 0,
+          "target": "2-stair"
+        },
+        {
+          "yaw": 1.293609612232844,
+          "pitch": 0.32762350885750635,
+          "rotation": 1.5707963267948966,
+          "target": "3-foyer"
+        },
+        {
+          "yaw": 2.1926070985846877,
+          "pitch": 0.3029734123105179,
+          "rotation": 1.5707963267948966,
+          "target": "5-courtyard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-courtyard",
+      "name": "courtyard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3224645720792663
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1339586606685756,
+          "pitch": 0.31935824612831176,
+          "rotation": 0,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
